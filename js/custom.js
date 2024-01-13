010e8b83985cc47363ba89456ec7d5dde68c2e93
TedoHarisC <tedoharischandra29@gmail.com>
2024-01-13 11:01:50 +0700

    Membuat Gallery untuk Halaman Customer

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+var $carousel = $('.gallery').flickity({
+    prevNextButtons: false,
+    pageDots: false
+});
+
+var flkty = $carousel.data('flickity');
+
+$('.button--previous').on('click', function () {
+    $carousel.flickity('previous');
+});
+// next
+$('.button--next').on('click', function () {
+    $carousel.flickity('next');
+});
